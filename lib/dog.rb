30010a2b23b9f77a703dc1495d235d6e18a4bdb5
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-16 19:19:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,14 @@
 class Dog
   def initialize(name)
     @name = name
-    @bark = "woof!"
   end
   def name
     @name
   end
   def name=(new_name)
     @name = new_name
-  end 
-  def bark
-    @bark
   end
-end 
+  def bark
+    puts "woof!"
+  end
+end
